2b4cdda9140f6b442d604d5671cdeff1351959dd
WolfWood72 <elaric@ngs.ru>
2018-10-25 05:57:35 +0700

    2
    2

--- Feistel.py
+++ Feistel.py
@@ -1,7 +1,8 @@
 from Scrembler import Scrembler
+import matplotlib.pyplot as plt
 
 class Feistel:
-    def __init__(self, block_size, type_key, type_func, num_rounds):
+    def __init__(self, block_size, type_key, type_func, num_rounds,change_bit_mode=None, change_bit_index=None):
         self.__block_size = block_size
         self.__type_key = type_key
         self.__num_rounds = num_rounds
@@ -13,10 +14,13 @@ class Feistel:
         }
         self.get_key = self.__key_dict[type_key]
         self.F = self.__functions_dict.get(type_func, self.__scrembler)
+        self.change_bit_mode = change_bit_mode
+        self.change_bit_index = change_bit_index
+        self.graph_info = None
 
 
     def __scrembler(self, V, X):
-        seq = Scrembler(init_value=[1,1,1,0,1,0,1], polynom=[0,1,14]).GetSequence(len(X))
+        seq = Scrembler(init_value=[0,0,0,0,0,0], polynom=[0,1,14]).GetSequence(len(X))
         S = [i^j for i,j in  zip(seq,X)]
         return [i^j for i,j in  zip(S,V)]
 
@@ -35,50 +39,96 @@ class Feistel:
         else:
             tmp = key[i:] + key[:i + 8 - len(key)]
         return Scrembler(init_value=tmp, polynom=[0,1]).GetSequence(self.__block_size)
+
+
     def __make_format(self, value, n_bit=15):
         _format = '0' + str(n_bit) + 'b'
         value.encode('utf-8')
         return [int(j) for j in ''.join(format(ord(i), _format) for i in value)]
 
 
-    def make_round(self, left, right,  key):
+
+    def __change_bit(self, text, ind):
+        if text[ind] == 1:
+            text[ind] = 0
+
+        else:
+            text[ind] = 1
+        return text
+
+
+    def change_info(self, text, key):
+        if self.change_bit_mode == 'messege':
+            return self.__change_bit(text, self.change_bit_index), key
+        elif self.change_bit_mode == 'key':
+            return text, self.__change_bit(key, self.change_bit_index)
+        else:
+            return text, key
+
+    def __make_round(self, left, right, key):
         N= max(len(right), len(left))
+       # print("left start = {}".format(left))
+        #print("right start = {}".format(right))
+        #print("key  = {}".format(key))
+
         s = self.F(key, left)
-        print(s)
+      #  print("s = {}".format(s))
         temp = [right[k]^s[k] for k in range(N)]
         right = left.copy()
         left = temp.copy()
+    #    print("temp  = {}".format(temp))
+
+     #   print("left end = {}".format(left))
+      #  print("right end = {}".format(right))
         return left, right
 
 
     def encoding(self, message, key):
+        def make_code(b_messege,key):
+            history_round = []
+            code = b_messege
+            length_mess = len(b_messege)
+            for i in range(self.__num_rounds):
+                temp = []
+                for n in range(0, length_mess, self.__block_size):
+                    block = code[n: n + self.__block_size]
+                    N = len(block) // 2
+                    Right = block[N:]
+                    Left = block[:N]
+                   # print("round {}".format(i))
+                    Right, Left = self.__make_round(Left.copy(), Right.copy(), self.get_key(key, i))
+                    temp += Left + Right
+                history_round.append(temp)
+                code = temp
+            return code, history_round
+
+
         key = self.__make_format(key)
         bit_message = self.__make_format(message)
-        length_mess = len(bit_message)
-        code = []
-        print(len(bit_message))
-        print(bit_message)
+
+
         NN = 0
         while len(bit_message)% self.__block_size != 0:
-            bit_message.append(1)
+            bit_message.append(0)
             NN += 1
-        for n in range(0,length_mess, self.__block_size):
-            block = bit_message[n: n + self.__block_size]
-            N = len(block)//2
-            Right = block[N:]
-            Left = block[:N]
-            print(n)
+
+        code, hist1 = make_code(bit_message,key)
+        #print("\n\n\n\n")
+        if self.change_bit_mode:
+            changed_mess, changed_key = self.change_info(bit_message, key)
+            changed_code,hist2 = make_code(changed_mess,changed_key)
+            self.graph_info = []
             for i in range(self.__num_rounds):
-                Right, Left = self.make_round(Left, Right, self.get_key(key,i))
-            code += Left + Right
+                self.graph_info.append(sum(map(lambda x: abs(x[0]-x[1]),list(zip(hist1[i],hist2[i])))))
         code = code[:-NN]
-        print(code)
+
+
         res = ""
         for i in range(0, len(code), 15):
             tmp = ''.join(str(j) for j in code[i: i + 15])
             res += chr(int(tmp, 2))
-        print(len(res))
-        print(res)
+        #print(len(res))
+       # print(res)
         return res
 
 
@@ -104,7 +154,7 @@ class Feistel:
             Left = block[:N]
 
             for i in range(self.__num_rounds-1,-1,-1):
-                Right, Left = self.make_round(Left, Right, self.get_key(key,i))
+                Right, Left = self.__make_round(Left, Right, self.get_key(key, i))
             mess += Left + Right
         print(len(mess))
         print(mess)
@@ -117,3 +167,10 @@ class Feistel:
         print(res)
             #code.append(Right)
         return res
+
+
+    def make_plot(self,X,Y):
+        plt.plot(X, Y)
+        plt.xlabel("round")  # Метка по оси x в формате TeX
+        plt.ylabel("bit")  # Метка по оси y в формате TeX
+        plt.savefig("plot.png")
--- main.py
+++ main.py
@@ -1,30 +1,108 @@
 import Feistel
+import argparse
+import sys
+
+type_key_list = ["cycle", "scrembler"]
+type_func_list = ["single", "scrembler"]
+change_bit_mode = ["messege", "key"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-encode", "-encode", action='store_true',
+                    help="input file", required=False)
+parser.add_argument("-decode","-decode",  action='store_true',
+                    help="input file", required=False)
+parser.add_argument("-input_file", type=str,
+                    help="file with input message")
+parser.add_argument("-output_file", type=str,
+                    help="file with output message")
+parser.add_argument("-key_file", type=str,
+                    help="file with key for Feistel net")
+
+parser.add_argument("-block_size", type=int,
+                    help="block_size",default=64)
+parser.add_argument("-num_rounds", type=int,
+                    help="num_rounds",default=10)
+parser.add_argument("-type_key", type=str,
+                    help="type_key  default cycle", choices=type_key_list,default="cycle")
+parser.add_argument("-type_func", type=str,
+                    help="type_func  default single",choices=type_func_list,default="single")
+parser.add_argument("-change_bit_mode", type=str,
+                    help="change_bit_mode",choices=change_bit_mode,default="key")
+parser.add_argument("-bit_index", type=int,
+                    help="bit_index default 5", default=5)
 
 
 
-messege = "Всего ничего"
-print(len(messege))
-key = "12345678564654654546564564646464556446"
-coder = Feistel.Feistel(block_size=64, type_key="scrembler", type_func="scrembler", num_rounds=16)
-code = coder.encoding(messege,key)
+namespace = parser.parse_args(sys.argv[1:])
 
 
-mess = coder.decoding(code , key)
+try:
+    if not namespace.decode and not namespace.encode:
+        raise ValueError("must have decode or encode param")
+    if namespace.decode and namespace.encode:
+        raise ValueError("must have only one of encode and deocde param")
 
-print(mess)
+    with open(namespace.input_file, "r", encoding="utf-8") as f:
+        messege = f.read()
 
-#a =  [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0]
-#
-#
-#def __make_format( value, n_bit=15):
-#    _format = '0' + str(n_bit) + 'b'
-#    value.encode('utf-8')
-#    return [int(j) for j in ''.join(format(ord(i), _format) for i in value)]
-#
-#
-#a = __make_format("asdadsasdasd!! вываы ыва#")
-#res = ""
-#for i in range(0, len(a),15):
-#    tmp = ''.join(str(j) for j in a[i: i + 15])
-#    res += chr(int(tmp, 2))
-#print(res)
+    with open(namespace.key_file, "r", encoding="utf-8") as f:
+        key = f.read()
+
+    block_size = namespace.block_size
+    type_key = namespace.type_key
+    type_func = namespace.type_func
+    num_rounds = namespace.num_rounds
+    change_bit_mode = namespace.change_bit_mode
+    change_bit_index = namespace.bit_index
+
+    coder = Feistel.Feistel(block_size=block_size, type_key=type_key, type_func=type_func, num_rounds=num_rounds,change_bit_mode=change_bit_mode, change_bit_index=change_bit_index)
+
+    if namespace.encode:
+        encoded_mes = coder.encoding(messege,key)
+        print("Закодированное сообщение: '{}'".format(encoded_mes))
+       # decoded_mes = coder.decoding(encoded_mes,key)
+      #  print(decoded_mes)
+        with open(namespace.output_file, "w", encoding="utf-8") as f:
+            f.write(encoded_mes)
+        coder.make_plot(list(range(len(coder.graph_info))), coder.graph_info)
+    elif namespace.decode:
+        decoded_mes = coder.decoding(messege,key)
+        print("Расшифрованное сообщение: '{}'".format(decoded_mes))
+
+
+        with open(namespace.output_file, "w", encoding="utf-8") as f:
+            f.write(decoded_mes)
+
+
+except Exception as e:
+    print(str(e))
+    exit("err")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
